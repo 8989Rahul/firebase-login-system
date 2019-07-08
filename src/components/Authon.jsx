97a4554f9af33c942d5fa774a48d438447d77ec3
8989Rahul <daghalerahul0@gmail.com>
2019-07-08 19:01:00 +0530

    install and initialize firebase

--- src/components/Authon.jsx
+++ src/components/Authon.jsx
@@ -1,7 +1,23 @@
 import React, { Component } from "react";
+var firebase = require("firebase");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBSbRrVuQfugFAeFjebieQM1FmAL1nue1k",
+  authDomain: "fir-login-171f0.firebaseapp.com",
+  databaseURL: "https://fir-login-171f0.firebaseio.com",
+  projectId: "fir-login-171f0",
+  storageBucket: "fir-login-171f0.appspot.com",
+  messagingSenderId: "695855483615",
+  appId: "1:695855483615:web:c15302bac30033cb"
+};
+firebase.initializeApp(firebaseConfig);
 
 export default class Authon extends Component {
-  login = () => {};
+  login = event => {
+    var email = this.refs.email.value;
+    var pass = this.refs.password.value;
+    console.log(email, pass);
+  };
   render() {
     return (
       <div>
